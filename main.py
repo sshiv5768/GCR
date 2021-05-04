457d0709b5cb40f1dd453171bf0b04aeb6ed0bb0
Shivlal Sharma <40575397+sshiv5768@users.noreply.github.com>
2021-05-04 22:29:28 +0530

    Added

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+
+st.write("""
+    # GCR Facilitator Student Progress Report:
+""")
+df = pd.read_csv('Progress.csv')
+tickerSymbol = st.text_input("Enter Your Name: ")
+st.write('Your Progress Report: ', tickerSymbol)
+
+if tickerSymbol:
+    df = df[df['Student Name'] == tickerSymbol]
+    st.write(df)
+else:
+    st.write('Data does not exits')
